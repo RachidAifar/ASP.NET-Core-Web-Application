c2853da073f2aef4558d81fcf06b2f5fbc787fcb
RachidAifar <90982692+RachidAifar@users.noreply.github.com>
2023-04-20 12:55:03 +0200

    feat<pro>:the Delete has been implemented

---
+++ Store_App/Store_App/Pages/Clients/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Store_App.Pages.Clients
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
